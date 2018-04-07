181c560180ce6916a2e89a94796abb6867327b0e
wov <wovfeng@gmail.com>
2018-04-07 23:13:12 +0800

    add

--- platforms/browser/www/main.bundle.js
+++ platforms/browser/www/main.bundle.js
@@ -1336,10 +1336,10 @@ var LoginService = /** @class */ (function () {
         this.messageService = messageService;
         this.domain = 'http://10.226.62.112:9080';
         this.loginUrl = 'http://10.226.62.112:9080/mfp/api/adapters/OA/authorization/login';
-        this.localLoginUrl = 'http://localhost:8080/mfp/api/adapters/OA/authorization/login';
+        this.localLoginUrl = 'http://192.168.3.113:8080/mfp/api/adapters/OA/authorization/login';
     }
     LoginService.prototype.login = function (uid, password) {
-        var url = this.localLoginUrl + "?uid=" + uid + "&password=" + password;
+        var url = this.loginUrl + "?uid=" + uid + "&password=" + password;
         return this.http.get(url)
             .pipe(Object(__WEBPACK_IMPORTED_MODULE_3_rxjs_operators__["c" /* timeout */])(3000), Object(__WEBPACK_IMPORTED_MODULE_3_rxjs_operators__["b" /* tap */])(function (_) { }), Object(__WEBPACK_IMPORTED_MODULE_3_rxjs_operators__["a" /* catchError */])(this.handleError('login', { "success": false,
             "error": {
